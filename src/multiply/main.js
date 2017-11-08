b855dea3ff79ece3228a38391fabc2713ee11386
Seunghoon Ko <imskojs@gmail.com>
2017-11-08 19:04:18 +0900

    multiply example

--- src/multiply/main.js
+++ src/multiply/main.js
@@ -1,1 +1,7 @@
 import { multiply } from 'ramda';
+
+
+// Now multiply function is globally available in `./build.js` file
+// Paste the content of `./build.js` to Solution pane of http://www.codewars.com/kata/multiply/train/javascript
+// Problem solved.
+// Bit of overkill here but just demonstrating.
