0b8de0520415d7b913b9d4c2952684b331ddd36b
Seunghoon Ko <imskojs@gmail.com>
2018-05-31 22:36:09 +0900

    6kyu example refactor using Ramda

--- src/6Kyu-MultiplesOf3Or5/main.js
+++ src/6Kyu-MultiplesOf3Or5/main.js
@@ -1,20 +1,16 @@
 import { range } from "rxjs";
 import { filter, reduce, tap } from "rxjs/operators";
+import * as R from "ramda";
+
+const isZero = R.equals(0);
 
 export default function solution(num) {
   let answer = 0;
   range(1, num - 1)
     .pipe(
-      filter(num => {
-        if (num % 3 === 0 || num % 5 === 0) {
-          return true
-        }
-      }),
-      reduce((accum, num) => {
-        return accum + num
-      }, 0)
+      filter(num => R.or(isZero(num % 3), isZero(num % 5))),
+      reduce((accum, num) => accum + num, 0)
     )
-
     .subscribe(result => answer = result)
 
   return answer;
