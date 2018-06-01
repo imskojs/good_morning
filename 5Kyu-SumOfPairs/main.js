1aae42ff674009d63af918251e6640edfe4ff64a
Seunghoon Ko <imskojs@gmail.com>
2018-06-01 22:41:03 +0900

    passing solution

--- 5Kyu-SumOfPairs/main.js
+++ 5Kyu-SumOfPairs/main.js
@@ -1,24 +1,36 @@
-import { from, of } from "rxjs";
-import { withLatestFrom, skip, scan, filter, take, concatMap, map } from "rxjs/operators";
+import { from } from "rxjs";
+import { scan, map, filter, take } from "rxjs/operators";
 import * as R from "ramda";
 
-export default function sum_pairs(ints, s) {
 
-  let solution = undefined;
+export default function sum_pairs(ints, s) {
+  let solution;
 
   from(ints).pipe(
-    skip(1),
-    scan((acc, val) => ({ val, i: R.inc(acc.i) }), { i: 0 }),
-    concatMap(obj => from(ints).pipe(
-      take(obj.i),
-      withLatestFrom(of(obj.val))
-    )),
-    filter(([front, last]) => front + last === s),
-    take(1),
+
+    scan((mem, val) => {
+      if (mem.val === undefined) { return { val }; }
+      let lastVal = mem.val;
+      mem[lastVal] = true;
+      mem.val = val;
+      return mem;
+    }, {}),
+
+    map(obj => {
+      let diff = s - obj.val
+      if (obj[diff] === true) { return [diff, obj.val] }
+      return []
+    }),
+
+    filter(array => array.length === 2),
+
+    take(1)
+
   )
     .subscribe(result => solution = result);
 
   return solution;
+
 }
 
 
