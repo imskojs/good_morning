7d53e711b6144d38d9656a941d250f6bd2b2b4e9
Seunghoon Ko <imskojs@gmail.com>
2018-12-28 10:44:07 +0900

    Recursive Replication

---
+++ 7Kyu-RecursiveReplication/main.ts
@@ -0,0 +1,16 @@
+import {range} from "rxjs";
+import {mapTo, toArray} from "rxjs/operators";
+
+export default function replicate(times: number, number: number) {
+    let solution: [] = [];
+
+    range(0, times).pipe(
+        mapTo(number),
+        toArray()
+    ).subscribe(
+        (array: []) =>  solution = array
+    );
+
+    return solution;
+
+}
