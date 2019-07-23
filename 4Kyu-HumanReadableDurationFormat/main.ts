f50abc8d34e5e35fe729e51531e56b3b6e3809a4
Seunghoon Ko <imskojs@gmail.com>
2019-07-23 09:26:40 +0900

    Human readable duration format, remove redundant if check

--- 4Kyu-HumanReadableDurationFormat/main.ts
+++ 4Kyu-HumanReadableDurationFormat/main.ts
@@ -10,36 +10,24 @@ interface Time {
 
 const getMinutes = (sec: number): Time => {
   const minute = Math.floor(sec / 60);
-  if (minute === 0) {
-    return {minute: 0, second: sec}
-  }
   const second = sec - 60 * minute;
   return {minute, second}
 };
 
 const getHours = (time: Time): Time => {
   const hour = Math.floor(time.minute / 60);
-  if (hour === 0) {
-    return {...time, hour: 0}
-  }
   const minute = time.minute - 60 * hour;
   return {...time, hour, minute}
 };
 
 const getDays = (time: Time): Time => {
   const day = Math.floor(time.hour / 24);
-  if (day === 0) {
-    return {...time, day: 0}
-  }
   const hour = time.hour - 24 * day;
   return {...time, day, hour}
 };
 
 const getYears = (time: Time): Time => {
   const year = Math.floor(time.day / 365);
-  if (year === 0) {
-    return {...time, year: 0}
-  }
   const day = time.day - 365 * year;
   return {...time, year, day}
 };
@@ -67,7 +55,6 @@ const format = ({second, minute, hour, day, year}: Time): string => {
     }, '');
 
 };
-
 
 const formatDuration = (sec: number) => compose(
   format,
